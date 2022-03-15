a88cae8c20ef5569bbacc93e0a267e5439a3b538
w40141 <w.40141@gmail.com>
2022-03-15 22:17:45 +0900

    edit

--- src/atoms/spin.rs
+++ src/atoms/spin.rs
@@ -4,13 +4,13 @@ use getset::Getters;
 #[getset(get = "pub")]
 pub struct Spin {
     symbol: Option<String>,
-    indexes: Option<Vec<i64>>,
     coefficient: i64,
-    value: S,
+    value: State,
 }
 
 #[derive(Debug)]
-pub enum S {
-    UP,
-    DOWN,
+pub enum State {
+    UP = 1,
+    Zero = 0,
+    DOWN = -1,
 }
